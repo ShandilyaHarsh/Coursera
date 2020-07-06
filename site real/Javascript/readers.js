0649d1d53c1f784335ed1d358fd6e1b3f1686ed3
ShandilyaHarsh <shandilyaharsh2001@gmail.com>
2020-07-06 18:07:57 +0530

    site

--- site real/Javascript/readers.js
+++ site real/Javascript/readers.js
@@ -1,11 +1,7 @@
-function validateForm()
- {
-  var x = document.forms["fname"].value;
-  if (x == "")
-   {
-    alert("Name must be filled out");
+function validateForm() {
+  var x = document.forms['fname'].value;
+  if (x == '') {
+    alert('Name must be filled out');
     return false;
-   }
- }
-
-
+  }
+}
